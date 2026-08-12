4a6c011c72c7b8d688a715475b3d2836789a3c44
Matheus <matheusmarketingmb@gmail.com>
2026-08-12 16:01:55 -0300

    Corrige fechamento do modal de equipe

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // Altere este identificador em toda publicação que modificar arquivos estáticos.
-const CACHE_NAME = "mibro-shell-20260812-10";
+const CACHE_NAME = "mibro-shell-20260812-11";
 const STATIC_ASSETS = [
   "/manifest.webmanifest",
   "/css/app.css",
